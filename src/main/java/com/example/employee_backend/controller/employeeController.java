02f0893ae2d662c732982a42b778073d05c35e88
vishnu1646 <vishnulance98@gmail.com>
2022-12-05 15:34:02 +0530

    base

---
+++ src/main/java/com/example/employee_backend/controller/employeeController.java
@@ -0,0 +1,8 @@
+package com.example.employee_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+public class employeeController {
+
+
+}
---
+++ src/test/java/com/example/employee_backend/EmployeeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employee_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
