d38cce5578b03ea913026bdc1df2536038927313
vishnu1646 <vishnulance98@gmail.com>
2022-12-05 15:38:41 +0530

    home

--- src/main/java/com/example/employee_backend/controller/employeeController.java
+++ src/main/java/com/example/employee_backend/controller/employeeController.java
@@ -2,7 +2,13 @@ package com.example.employee_backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class employeeController {
+    @GetMapping("/")
+     public String Homepage(){
+        return "Welcome to employee page";
+    }
+
 
 
 }
