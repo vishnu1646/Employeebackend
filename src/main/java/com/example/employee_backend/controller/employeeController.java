2a43a3940dd06c962ebc4697938a9b615a0e4d9c
vishnu1646 <vishnulance98@gmail.com>
2022-12-06 10:33:28 +0530

    reading

--- src/main/java/com/example/employee_backend/controller/employeeController.java
+++ src/main/java/com/example/employee_backend/controller/employeeController.java
@@ -1,36 +1,27 @@
 package com.example.employee_backend.controller;
 
+import com.example.employee_backend.controller.model.employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class employeeController {
-    @GetMapping("/")
-     public String Homepage(){
-        return "Welcome to employee page";
-    }
 
-     @GetMapping("/add")
-    public String Adding(){
+     @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
+    public String Adding(@RequestBody employee e){
+         System.out.println(e.getName().toString());
+         System.out.println(e.getEmpcode());
+         System.out.println(e.getDesignation());
+         System.out.println(e.getSalary());
+         System.out.println(e.getCmpnyname());
+         System.out.println(e.getPhonenum());
+         System.out.println(e.getUsername());
+         System.out.println(e.getPassword());
         return "Adding an employee";
      }
-     @GetMapping("/search")
-    public String Searching(){
-        return  "Searching an employee";
-     }
 
-     @GetMapping("/edit")
-    private String edit(){
-        return "Editing an employee";
-     }
 
-     @GetMapping("/viewall")
-    private String view(){
-        return "Viewing Employees";
-     }
 
-     @GetMapping("/delete")
-    public String delete(){
-        return "Deleting an Employee";
-     }
 
 }
---
+++ src/main/java/com/example/employee_backend/controller/model/employee.java
@@ -0,0 +1,110 @@
+package com.example.employee_backend.controller.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+public class employee {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private int empcode;
+    private String name;
+    private String designation;
+    private int Salary;
+    private String cmpnyname;
+    private int phonenum;
+    private String username;
+    private String password;
+
+    public employee() {
+    }
+
+    public employee(int id, int empcode, String name, String designation, int salary, String cmpnyname, int phonenum, String username, String password) {
+        this.id = id;
+        this.empcode = empcode;
+        this.name = name;
+        this.designation = designation;
+        this.Salary = salary;
+        this.cmpnyname = cmpnyname;
+        this.phonenum = phonenum;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEmpcode() {
+        return empcode;
+    }
+
+    public void setEmpcode(int empcode) {
+        this.empcode = empcode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public int getSalary() {
+        return Salary;
+    }
+
+    public void setSalary(int salary) {
+        Salary = salary;
+    }
+
+    public String getCmpnyname() {
+        return cmpnyname;
+    }
+
+    public void setCmpnyname(String cmpnyname) {
+        this.cmpnyname = cmpnyname;
+    }
+
+    public int getPhonenum() {
+        return phonenum;
+    }
+
+    public void setPhonenum(int phonenum) {
+        this.phonenum = phonenum;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
